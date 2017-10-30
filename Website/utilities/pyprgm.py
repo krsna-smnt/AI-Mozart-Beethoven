61e0a6e8956be571cfc1bc7723f27124eafba8fa
Abhay <abhaytyagi1998@gmail.com>
2017-10-30 12:56:46 +0530

    Merged

--- Website/utilities/pyprgm.py
+++ Website/utilities/pyprgm.py
@@ -17,18 +17,15 @@ def play_music(music_file):
     except pg.error:
         print("File {} not found! {}".format(music_file, pg.get_error()))
         return
+
     pg.mixer.music.play()
     # check if playback has finished
     while pg.mixer.music.get_busy():
         clock.tick(30)
 
 
-def play(file):
-	# pick a midi or MP3 music file you have in the working folder
-	# or give full pathname
-	# music_file = "/home/abhay/Desktop/moz2.mid" 
+def play(file): 
 	music_file = "/home/abhay/Desktop/Django/MusicApp/" + file
-	#music_file = "Drumtrack.mp3"
 
 	freq = 44100    # audio CD quality
 	bitsize = -16   # unsigned 16 bit
@@ -36,14 +33,11 @@ def play(file):
 	buffer = 2048   # number of samples (experiment to get right sound)
 	pg.mixer.init(freq, bitsize, channels, buffer)
 
-	# optional volume 0 to 1.0
 	pg.mixer.music.set_volume(0.8)
 
 	try:
 	    play_music(music_file)
 	except KeyboardInterrupt:
-	    # if user hits Ctrl/C then exit
-	    # (works only in console mode)
 	    pg.mixer.music.fadeout(1000)
 	    pg.mixer.music.stop()
-	    raise SystemExit
+	    # raise SystemExit
